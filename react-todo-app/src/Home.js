4d6b386419948d7cc9c1195a2b3d0e8ac58c607c
liju <liju.jacob08@gmail.com>
2021-11-23 02:27:56 +0530

    Lesson 11 complete

--- react-todo-app/src/Home.js
+++ react-todo-app/src/Home.js
@@ -1,11 +1,12 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
   
   const [blogs,setBlogs]= useState([
-    {titile:'My new website', body: 'lorem ipsum..',author:'mario',id:1},
-    {titile:'Welcome Party', body: 'lorem ipsum..',author:'Yoshi',id:2},
-    {titile:'Web dev top tips', body: 'lorem ipsum..',author:'mario',id:3},
+    {title:'My new website', body: 'lorem ipsum..',author:'mario',id:1},
+    {title:'Welcome Party', body: 'lorem ipsum..',author:'Yoshi',id:2},
+    {title:'Web dev top tips', body: 'lorem ipsum..',author:'mario',id:3},
 
   ]);      
   
@@ -13,13 +14,7 @@ const Home = () => {
   
     return (    
         <div className="home">
-           {blogs.map((blog) => (
-               <div className="blog-preview" key={blog.id} > 
-                <h2>{blog.titile}</h2>
-                <p>written by {blog.author}</p>
-               </div>
-            ))
-            }  
+             <BlogList blogs ={blogs} title ="All Blogs!" />
         </div>
 
       );
