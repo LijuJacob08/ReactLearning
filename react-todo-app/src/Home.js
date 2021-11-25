69fb4777229cb6050c96e3624753ec07f98df98c
liju <liju.jacob08@gmail.com>
2021-11-25 23:58:53 +0530

    lesson-17 Fetching data using useEffect

--- react-todo-app/src/Home.js
+++ react-todo-app/src/Home.js
@@ -3,28 +3,22 @@ import BlogList from './BlogList';
 
 const Home = () => {
   
-  const [blogs,setBlogs]= useState([
-    {title:'My new website', body: 'lorem ipsum..',author:'Mario',id:1},
-    {title:'Welcome Party', body: 'lorem ipsum..',author:'Yoshi',id:2},
-    {title:'Web dev top tips', body: 'lorem ipsum..',author:'Mario',id:3},
+  const [blogs,setBlogs]= useState(null); 
 
-  ]);      
-  const [name,setName]= useState("Mario");
-
-  const handleDelete =(id)=>{
-    const newBlogs= blogs.filter(blog=>blog.id!==id);
-    setBlogs(newBlogs);
-    } 
-     useEffect(()=>{
-         console.log("use effect ran");
-         console.log(name);
-     },[name]);
+ 
+     useEffect(()=>{fetch('http://localhost:8000/blogs')
+     .then(res => {
+       return res.json();
+     })
+     .then(data => {
+       setBlogs(data);
+     })
+ }, []) 
   
     return (    
         <div className="home">
-             <BlogList blogs ={blogs} title ="All Blogs!" handleDelete ={handleDelete} />
-             <button onClick={()=>{setName("luigi")}}>Change Name</button>               
-                <p>{name}</p>
+               {blogs &&  <BlogList blogs ={blogs} /> }            
+                
             </div>
 
       );
