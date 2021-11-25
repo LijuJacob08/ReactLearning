a7959889291e68be46ed6ce0d398ebc4c9909336
liju <liju.jacob08@gmail.com>
2021-11-25 16:39:50 +0530

    lesson15 useEffect dependencies

--- react-todo-app/src/Home.js
+++ react-todo-app/src/Home.js
@@ -9,19 +9,22 @@ const Home = () => {
     {title:'Web dev top tips', body: 'lorem ipsum..',author:'Mario',id:3},
 
   ]);      
+  const [name,setName]= useState("Mario");
+
   const handleDelete =(id)=>{
     const newBlogs= blogs.filter(blog=>blog.id!==id);
     setBlogs(newBlogs);
     } 
      useEffect(()=>{
          console.log("use effect ran");
-         console.log(blogs);
-     });
+         console.log(name);
+     },[name]);
   
     return (    
         <div className="home">
              <BlogList blogs ={blogs} title ="All Blogs!" handleDelete ={handleDelete} />
-            
+             <button onClick={()=>{setName("luigi")}}>Change Name</button>               
+                <p>{name}</p>
             </div>
 
       );
