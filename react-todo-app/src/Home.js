a1ba27a6fa24101b428395540cae8e1340762dcd
liju <liju.jacob08@gmail.com>
2021-11-26 08:13:03 +0530

    Handling Fetch Errors

--- react-todo-app/src/Home.js
+++ react-todo-app/src/Home.js
@@ -5,24 +5,35 @@ const Home = () => {
   
   const [blogs,setBlogs]= useState(null); 
   const [isPending,setIsPending]= useState(true);  
- 
+  const [error,setError] =useState(null);  
      useEffect(()=>{
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-     .then(res => {
-       return res.json();
-     })
-     .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-        });
-        },1000)
+            .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource');
+                }
+            return res.json();
+            })
+            .then(data => {
+               
+                setBlogs(data);
+                setError(null);
+                setIsPending(false);
+                })
+            .catch(err=>{
+               
+                setIsPending(false);
+                setError(err.message);
+                 })
+                },1000)
      }, []) 
   
     return (    
         <div className="home">
-                {isPending && <div>Loading...</div>}
-               {blogs &&  <BlogList blogs ={blogs} /> }            
+            {error && <div> {error}</div>}    
+            {isPending && <div>Loading...</div>}
+            {blogs &&  <BlogList blogs ={blogs} /> }            
                 
             </div>
 
