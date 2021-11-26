f21be55dfd001d45153ae0eee683d9a8e6017d6b
liju <liju.jacob08@gmail.com>
2021-11-26 18:03:57 +0530

    useEffect Cleanup

--- react-todo-app/src/useFetch.js
+++ react-todo-app/src/useFetch.js
@@ -3,9 +3,11 @@ const useFetch = (url) =>{
     const [data,setData]= useState(null); 
     const [isPending,setIsPending]= useState(true);  
     const [error,setError] =useState(null);  
+    
        useEffect(()=>{
-          setTimeout(() => {
-              fetch(url)
+        const abortCont = new AbortController();
+        setTimeout(() => {
+              fetch(url,{signal:abortCont.signal})
               .then(res => {
                   if(!res.ok){
                       throw Error('could not fetch the data for that resource');
@@ -19,11 +21,15 @@ const useFetch = (url) =>{
                   setIsPending(false);
                   })
               .catch(err=>{
-                 
+                 if(err.name==='AbortError'){
+                    console.log("Fetch Aborted");
+                 }else{
                   setIsPending(false);
                   setError(err.message);
-                   })
+                 }
+                })
                   },1000)
+                  return ()=>abortCont.abort();
        }, [url]) 
        return{data,isPending,error};
     }
