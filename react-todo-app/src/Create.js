a27c6fca3ece1785f01b5db4133b851b6570da7e
liju <liju.jacob08@gmail.com>
2021-11-27 12:18:53 +0530

    Controlled Inputs (forms)

--- react-todo-app/src/Create.js
+++ react-todo-app/src/Create.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 const Create =()=>{
 
+    const [title,setTitle]=useState('');
+    const [body,setBody]= useState('');
+    const [author,setAuthor] = useState('Mario');
+    
     return(
         <div className="create">
         <h2>Add a new blog</h2>
+        <form>
+            <label>Blog Title</label>
+        <input type='text' value={title} required
+            onChange={(e)=>{setTitle(e.target.value)}}
+        />
+        <label>Blog Body</label>
+        <textarea value={body} required 
+        onChange ={(e)=>{setBody(e.target.value)}}
+        ></textarea>
+        <label>Blog Author</label>
+        <select value ={author} onChange ={(e)=>{setAuthor(e.target.value)}} >  
+            <option value='Mario'>Mario</option>
+            <option value='Yoshi'>Yoshi</option>
+        </select>
+        <button>Add a new blog</button>  
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+        </form>
+
+
         </div>
     );
 
